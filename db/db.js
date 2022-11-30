b5c3f29a1875b113f887e956a0d5046d322d04b5
cjiaxin9 <chuajiaxin9@gmail.com>
2022-11-30 17:18:53 +0800

    connected to render database

--- db/db.js
+++ db/db.js
@@ -1,14 +1,20 @@
 const { Client } = require("pg");
 
 const client = new Client({
-  host: "localhost",
+  host: "dpg-ce3h9a2rrk02ufihqma0-a.singapore-postgres.render.com",
   port: "5432",
-  user: "db_user",
-  password: "123456",
+  user: "vendingworld_user",
+  password: "J7lO1QmzfFHtUf2yLTY9m7HLlM480o3k",
   database: "vendingworld",
+  ssl: true,
+  // host: "localhost",
+  // port: "5432",
+  // user: "db_user",
+  // password: "123456",
+  // database: "vendingworld",
 });
 
-client.connect((err) => {
+postgres: client.connect((err) => {
   if (err) {
     console.error("connection error", err.stack);
   } else {
